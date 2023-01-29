da3dbac5b517dd46593142efa8b7db273d73a949
Simone Surace <simone.surace@artificialy.com>
2023-01-29 22:33:44 +0100

    Add simple example with KernelFunctions

--- src/SnoopPrecompileMWE.jl
+++ src/SnoopPrecompileMWE.jl
@@ -1,48 +1,46 @@
 module SnoopPrecompileMWE
 
-using LinearAlgebra
-using Zygote: gradient
+using KernelFunctions
+using KernelFunctions: kappa, metric
+using Zygote: gradient, @ignore
+
+function KernelFunctions.kernelmatrix(
+    κ::KernelFunctions.SimpleKernel, x::AbstractVector, y::AbstractVector
+)
+    KernelFunctions.validate_inputs(x, y)
+    dist = metric(κ)
+    return kappa.(Ref(κ), dist.(x, y'))
+end
+
+using SnoopPrecompile
 
 struct Loss{Tx, Ty}
     x::Tx
     y::Ty
 end
 
-sqdist(x::Real, y::Real) = (x-y)^2
-sqdist(x, y) = dot(x, x) + dot(y, y) - 2dot(x, y)
-sekernel(v, l, x, y) = v * exp(-sqdist(x, y) / (2l^2))
-
 function (l::Loss)(θ)
-    # k = θ.variance * SEKernel() ∘ ScaleTransform(1/θ.lengthscale)
-    # return sum(kernelmatrix(k, l.x, l.y))
-    return sum(sekernel.(θ.variance, θ.lengthscale, l.x, permutedims(l.y)))
+    k = θ.variance * SEKernel() ∘ ScaleTransform(θ.lengthscale)
+    return sum(kernelmatrix(k, l.x, l.y))
 end
 
+# This function compiles when the precompilation block below is commented out,
+# but it generates an error 
+# "ERROR: LoadError: Mutating arrays is not supported"
+# when called within the precompilation block.
 function compute_gradient(l, θ)
     return only(gradient(l, θ))
 end
 
 export Loss, compute_gradient
 
-using SnoopPrecompile
-using KernelFunctions: RowVecs, ColVecs
-
 @precompile_setup begin
-    losses = [
-        Loss(randn(10), randn(10)),
-        Loss(RowVecs(randn(10, 5)), RowVecs(randn(10, 5))),
-        Loss(eachrow(randn(10, 5)), eachrow(randn(10, 5))),
-        Loss(ColVecs(randn(10, 5)), ColVecs(randn(10, 5))),
-        Loss(eachcol(randn(10, 5)), eachcol(randn(10, 5))),
-        Loss([randn(5) for _ in  1:10], [randn(5) for _ in  1:10])
-    ]
+    l = Loss(randn(10), randn(10))
     θ = (variance = 1., lengthscale = 1.)
 
     @precompile_all_calls begin
-        for loss in losses
-            loss(θ)
-            compute_gradient(loss, θ)
-        end
+        l(θ)
+        compute_gradient(l, θ)
     end
 end
 
